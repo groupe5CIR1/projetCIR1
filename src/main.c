6b137b3695c3160e07c8e0d6de10a860a91eaa1f
Gabriel <gabrielpagniez06@gmail.com>
2025-06-12 14:59:50 +0200

    obtention id + title & html dans export

--- src/main.c
+++ src/main.c
@@ -1,15 +1,29 @@
 #include "main.h"
 
 void main(void) {
+    FILE *f = fopen("book.txt", "r");
+    if (!f) {
+        perror("Error opening file");
+        exit(1);
+    }
+    char line[LINE_SIZE];
+    while(fgets(line, sizeof(line), f)) {
+        if (strstr(line, "<chapter") != NULL) {
+            char id[128];
+            char title[256];
+            sscanf(line, "<chapter id=\"%[^\"]\">%[^<]", id, title);
+            printf("id = %s\n",id);
+            printf("title = %s\n",title);
+            FILE* file = create_file(id);
+            }  
+        }
+    }
 
-}
 
-
-
-FILE* create_file(char* id){
-    char filename[128];
-    snprintf(filename, sizeof(filename), "%s.html", id);
-    FILE *file = fopen(id, "w");
+FILE* create_file(char* id){    
+    char filename[256];
+    snprintf(filename, sizeof(filename), "export/%s.html", id);
+    FILE *file = fopen(filename, "w");
     if (!file) {
         perror("Error opening file");
         exit(1);
