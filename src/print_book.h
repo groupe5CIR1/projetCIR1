1cad5a4db5a20fa04a318ff2dfd1655f90cf8fce
Valentin TANG-PATUREL <valentin.tang-paturel@isen-ouest.yncrea.fr>
2025-06-13 11:16:04 +0200

    uwu

--- src/print_book.h
+++ src/print_book.h
@@ -7,10 +7,10 @@
 
 #define LINE_SIZE 512
 #define BOOK "book.txt"
-#endif
 
 void print_book();
 FILE* create_file(char* filename);
 void init_file(FILE* file, char* id);
 void end_file(FILE* file);
 void print_line(FILE *file, char* line);
+#endif
