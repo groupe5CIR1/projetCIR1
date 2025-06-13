18f887ba7402c430aafecef33f24f95154aaa677
Gabriel <gabrielpagniez06@gmail.com>
2025-06-13 16:38:48 +0200

    entity

--- src/entity.c
+++ src/entity.c
@@ -4,7 +4,24 @@ For armor management, see armor.c
 For inventory management, see inventory.c
 */
 
+#include "entityArray.h"
 #include "entity.h"
 
-
+struct Entity entity_init(int type){
+    float health;
+    switch (type)
+    {
+    case PLAYER:
+        health = 100.0;
+        break;
+    case MONSTER:
+        health = 70.0;
+        break;
+    case ENNEMY:
+        health = 90.0;
+        break;
+    }
+    //il faudra init inventory et possiblement uid (shield et armor si on a le temps)
+    return (struct Entity) {.uid = NULL, .loaded = 1, .type = type , .health = health, .defaultDamage = NULL, .shield = 0, };
+}
 
--- src/entity.h
+++ src/entity.h
@@ -4,25 +4,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdbool.h>
 
 #include "armor.h"
 #include "inventory.h"
 
 #define MAX_SLOTS 6
 
+enum Type{
+    PLAYER,
+    MONSTER,
+    ENNEMY,
+};
 
 struct Entity {
     int uid;
     bool loaded;
-    char* type;
-    unsigned int health;
+    int type;
+    float health;
     int defaultDamage;
     bool shield;
     struct Armor* armor;
     struct Item* inventory;
 };
 
-struct Entity* init_entity(char* entity);
+
+struct Entity init_entity(int type);
 
 void damage(struct Entity* entity, struct Entity* entity2);
 
